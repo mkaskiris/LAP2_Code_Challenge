54d8b839f7fba7d6ce7a77dac7471b45c9337c50
Menelaos <mkaskiris@hotmail.com>
2021-12-08 11:06:07 +0000

    route status msg

--- server/api/controllers/posts.js
+++ server/api/controllers/posts.js
@@ -6,17 +6,17 @@ const Post = require('../models/post');
 router.get('/:id', async (req,res) =>{
     try{
         const post = await Post.findByPath(parseInt(req.params.id));
-        res.json(post)
+        res.json(post);
     }catch(err){
-        res.status(404).json({err})
+        res.status(404).json({err});
     }
 })
 
 router.post('/', async (req,res) =>{
     try{
-        const post = await Post.create(req.body.title, req.body.name, req.body.body)
-        res.json(post);
+        const post = await Post.create(req.body.title, req.body.name, req.body.body);
+        res.status(201).json(post);
     }catch (err){
-        res.status(404).json({err})
+        res.status(404).json({err});
     }
 })
