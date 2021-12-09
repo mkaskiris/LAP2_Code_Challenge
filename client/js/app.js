f5f7edfb32a62c204482bde461d6ac1ab4a4b5c9
Menelaos <mkaskiris@hotmail.com>
2021-12-09 10:20:16 +0000

    front end

--- client/js/app.js
+++ client/js/app.js
@@ -1,4 +1,5 @@
 const btn = document.querySelector('#post-form');
+const main = document.querySelector('main');
 
 function submitPost(e){
     e.preventDefault();
@@ -11,21 +12,41 @@ function submitPost(e){
     const options = {
         method: 'POST',
         body: JSON.stringify(postData),
-        redirect:  'follow',
         headers: {
             "Content-Type": "application/json"
         }
     };
 
     fetch('http://localhost:3000/posts/', options)
-        //.then(d=>d.json())
-        .then(window.location.href = 'www.google.com')
+        .then(d=>d.json())
+        .then(d => window.location.hash = `/posts/${d.id}`)
         .catch(console.warn)
-        // .then(getID)
-        // .then(window.location.replace(''))
 }
 
 btn.addEventListener('submit', submitPost);
+window.addEventListener('hashchange', updateContent)
+
+function updateContent(){
+    let hash = window.location.hash.substring(8);
+    updateMain(hash);
+}
+
+function updateMain(hash){
+    main.innerHTML = '';
+    const title = document.createElement('h1');
+    const name = document.createElement('h2');
+    const text = document.createElement('p');
+    fetch(`http://localhost:3000/posts/${hash}`)
+        .then(d=>d.json())
+        .then(d => {
+            title.textContent = d.title;
+            name.textContent = d.name;
+            text.textContent = d.body;
+        })
+    main.appendChild(title);
+    main.appendChild(name);
+    main.appendChild(text);
+}
 
 console.log('what up')
 
